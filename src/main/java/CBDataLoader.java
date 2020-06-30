56ffbfc52bf9c5796a26b8d8f0ba0137451aae9a
Denis <firsa.denis@gmail.com>
2020-06-30 20:25:08 +0300

    add parsing info by CB + test coverage 4 it

--- src/main/java/CBDataLoader.java
+++ src/main/java/CBDataLoader.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class CBDataLoader {
+
+    private static final String urlPattern = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=";
+
+    public static String getRate(String date) throws IOException {
+        Document doc = Jsoup.connect(urlPattern + date).get();
+        Elements valutes = doc.select("Valute");
+
+        String result = "";
+        for (Element el : valutes){
+            String valute = el.select("CharCode").text();
+            if(Checker.hasValute(valute)){
+                double nominal = Double.valueOf(el.select("Nominal").text());
+                double value = Double.valueOf(el.select("Value").text().replace(',','.'));
+                result += valute + " : " + value / nominal + "\n";
+            }
+        }
+
+        return "Курс ЦБ РФ\n" + result;
+    }
 }
--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Checker {
+
+    private static final String[] valutes = {"USD", "EUR", "GBP", "CNY", "JPY"};
+
+    public static boolean hasValute(String val){
+        return Arrays.asList(valutes).contains(val);
+    }
 }
